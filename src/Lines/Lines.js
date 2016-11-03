d41f9d411188c6e96ba2fd7ecdd8bacf05c1715e
Nick Rutten <hello@nickrttn.me>
2016-11-03 16:48:09 +0100

    preliminary tooltip, no styling

--- src/Lines/Lines.js
+++ src/Lines/Lines.js
@@ -2,20 +2,25 @@
 import React, { Component, PropTypes } from 'react';
 import { axisBottom, scaleTime, select } from 'd3';
 import moment from 'moment';
+import 'moment/locale/nl';
 
 // Components
 import Legend from '../Legend/Legend';
 
 // Assets
 import './Lines.css';
-// import star from './svg/star.svg';
-// import cross from './svg/cross.svg';
+import star from './svg/star.svg';
+import cross from './svg/cross.svg';
 // import monument from './svg/monument.svg';
 
 class Lines extends Component {
   static props = {
     data: PropTypes.array.isRequired,
     handleLineClick: PropTypes.func.isRequired,
+  }
+
+  state = {
+    tooltip: false,
   }
 
   componentDidMount() {
@@ -63,8 +68,24 @@ class Lines extends Component {
     />
   }
 
+  showTooltip = (d) => {
+    this.setState({
+      tooltip: {
+        name: d.name,
+        birth: d.birthdate.format('DD MMMM YYYY'),
+        death: d.dateOfDeath.format('DD MMMM YYYY'),
+        profession: d.profession,
+      }
+    });
+  }
+
+  hideTooltip = () => {
+    this.setState({ tooltip: false });
+  }
+
   render() {
     const { data } = this.props;
+    const { tooltip } = this.state;
 
     return (
       <section className="lines">
@@ -76,13 +97,22 @@ class Lines extends Component {
           <svg width="100%" height={ (data.length * 12) + 30 }>
             { data.length > 0 && this.renderWarRect() }
             { data.length > 0 && data.map((d, i) =>
-              <Line key={ d.id } x={ this.state.x } d={d} i={i} handleClick={ this.props.handleLineClick } />
+              <Line key={ d.id } x={ this.state.x } d={d} i={i} hideTooltip={ this.hideTooltip } showTooltip={ this.showTooltip } handleClick={ this.props.handleLineClick } />
             )}
           </svg>
           <svg className="axis" width='78vw' height="50px">
             <g ref={ ref => this._axis = ref }
               transform="translate(0, 10)"></g>
           </svg>
+          { tooltip &&
+            <div className="tooltip">
+              <h4>{ tooltip.name }</h4>
+              <p>{ tooltip.profession }</p>
+              <p><img src={ star } alt="Geboortedatum" /> { tooltip.birth }</p>
+              <p><img src={ cross } alt="Sterfdatum" /> { tooltip.death }</p>
+              <p>Klik op de lijn om het dossier te bekijken</p>
+            </div>
+          }
         </section>
       </section>
     );
@@ -100,6 +130,8 @@ const Line = (props) => (
       height="12px"
       fill="transparent"
       onClick={ () => props.handleClick(props.d.id) }
+      onMouseOver={ () => props.showTooltip(props.d) }
+      onMouseOut={ () => props.hideTooltip() }
     />
     <line
         className="lines__line"
